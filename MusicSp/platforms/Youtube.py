2b6a2407beca925ed8d2a648fea29118f8e12cba
simplesaniyaa <simplesaniyaa@gmail.com>
2026-09-07 00:30:51 +0530

    Update Youtube.py

--- MusicSp/platforms/Youtube.py
+++ MusicSp/platforms/Youtube.py
@@ -10,7 +10,7 @@ import aiohttp
 
 API_URL = os.environ.get("SHRUTI_API_URL", "https://api.shrutibots.site")
 
-API_KEY = os.environ.get("SHRUTI_API_KEY", "ShrutiBotsAmUYpnPtrqCbWdBZn7Mj") ## Get This API KEY FROM TELEGRAM BOT USERNAME: @SHRUTIAPIBOT 
+API_KEY = os.environ.get("SHRUTI_API_KEY", "ShrutiBotsWjg5i1RGESHyP3Z2PWXu") ## Get This API KEY FROM TELEGRAM BOT USERNAME: @SHRUTIAPIBOT 
 
 DOWNLOAD_DIR = "downloads"
 
